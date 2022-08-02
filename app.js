e0f0d3ea3279a9842ecafc0d96bdc5474ce9265b
brobin714 <brobin714@gmail.com>
2022-08-02 07:21:09 -0400

    fafafaf

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ main().catch(err => console.log(err));
 
 
 async function main() {
-const password = process.env.SECRET_KEY
+const password = process.env.SECRET_KEY;
   await mongoose.connect('mongodb+srv://brobin714:'+ password +'@cluster0.qcwip.mongodb.net/todolistDB');
 }
 
